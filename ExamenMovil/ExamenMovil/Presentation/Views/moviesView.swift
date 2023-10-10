f592e4d59aff8809d49c2bb7579769f970903b38
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-10-10 13:21:28 -0600

    feat: add UI and local data

--- ExamenMovil/ExamenMovil/Presentation/Views/moviesView.swift
+++ ExamenMovil/ExamenMovil/Presentation/Views/moviesView.swift
@@ -14,6 +14,32 @@ struct MovieListView: View {
     
     // Track the index of the selected movie
     @State private var selectedMovieIndex = 0
+    
+    // Define an array of local data for the circles
+    let circleData: [CircleData] = [
+        CircleData(color: Color.red, text: "Circle 1"),
+        CircleData(color: Color.green, text: "Circle 2"),
+        CircleData(color: Color.blue, text: "Circle 3"),
+        CircleData(color: Color.red, text: "Circle 4"),
+        CircleData(color: Color.green, text: "Circle 5"),
+        CircleData(color: Color.blue, text: "Circle 6"),
+        CircleData(color: Color.red, text: "Circle 7"),
+        CircleData(color: Color.green, text: "Circle 8"),
+        CircleData(color: Color.blue, text: "Circle 9"),
+        CircleData(color: Color.red, text: "Circle 10"),
+        CircleData(color: Color.green, text: "Circle 11"),
+        CircleData(color: Color.blue, text: "Circle 12"),
+        CircleData(color: Color.red, text: "Circle 13"),
+        CircleData(color: Color.green, text: "Circle 14"),
+        CircleData(color: Color.blue, text: "Circle 15"),
+        CircleData(color: Color.red, text: "Circle 16"),
+        CircleData(color: Color.green, text: "Circle 17"),
+        CircleData(color: Color.blue, text: "Circle 18"),
+        CircleData(color: Color.red, text: "Circle 19"),
+        CircleData(color: Color.green, text: "Circle 20"),
+        CircleData(color: Color.blue, text: "Circle 21"),
+        
+    ]
 
     var body: some View {
         // Create a ZStack to overlay views
@@ -22,6 +48,7 @@ struct MovieListView: View {
             LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             
+            
             // Create a vertical stack for the main content
             VStack {
                 Text("Trending Movies")
@@ -29,54 +56,44 @@ struct MovieListView: View {
                     .foregroundColor(.white)
                     .padding(.top, 16)
                 
+                
                 // Create a horizontal scroll view for displaying movie circles
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 16) {
-                        ForEach(viewModel.movies.indices, id: \.self) { index in
+                        ForEach(circleData.indices, id: \.self) { index in
                             GeometryReader { geometry in
-                                // Create a circle for each movie
-                                Circle()
-                                    .frame(width: circleSize(for: geometry.frame(in: .global).midX, index: index), height: circleSize(for: geometry.frame(in: .global).midX, index: index))
-                                    .foregroundColor(Color.gray)
-                                    .opacity(1.0)
-                                    .offset(y: 0)
-                                    .scaleEffect(selectedMovieIndex == index ? 1.2 : 1.0)
-                                    .animation(.easeInOut)
-                                    .onTapGesture {
-                                        withAnimation {
-                                            selectedMovieIndex = index
+                                // Create a ZStack to overlay Circle and Text
+                                ZStack {
+                                    Circle()
+                                        .frame(width: circleSize(for: geometry.frame(in: .global).midX, index: index), height: circleSize(for: geometry.frame(in: .global).midX, index: index))
+                                        .foregroundColor(circleData[index].color)
+                                        .opacity(1.0)
+                                        .offset(y: 0)
+                                        .scaleEffect(selectedMovieIndex == index ? 1.2 : 1.0)
+                                        .animation(.easeInOut)
+                                        .onTapGesture {
+                                            withAnimation {
+                                                selectedMovieIndex = index
+                                            }
                                         }
-                                    }
-                                    .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 2) // Add shadow here
+                                        .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 2) // Add shadow here
+                                    
+                                    Text(circleData[index].text)
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 14))
+                                }
                             }
-                            .frame(width: 100, height: 100)
+                            .frame(width: 100, height: 600)
                         }
                     }
                 }
                 .padding(16)
                 
-                // Create a horizontal scroll view for displaying circle indicators
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 16) {
-                        ForEach(0..<10, id: \.self) { index in
-                            // Create a circle indicator
-                            Circle()
-                                .frame(width: circleSize(for: UIScreen.main.bounds.width / 2, index: index), height: circleSize(for: UIScreen.main.bounds.width / 2, index: index))
-                                .foregroundColor(Color.white)
-                                .opacity(1.0)
-                                .offset(y: 0)
-                                .scaleEffect(1.0)
-                                .animation(.easeInOut)
-                                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 2) // Add shadow here
-                        }
-                    }
-                }
-                .padding(16)
             }
         }
         .onAppear(perform: {
             // Load movie data when the view appears
-            viewModel.loadData()
+            //viewModel.loadData() // You can comment out this line if you're using local data
         })
     }
 
@@ -92,4 +109,10 @@ struct MovieListView: View {
 
         return index == selectedMovieIndex ? maxCircleSize : minCircleSize + (maxCircleSize - minCircleSize) * scale
     }
+}
+
+// Struct to represent circle data including color and text
+struct CircleData {
+    let color: Color
+    let text: String
 }
