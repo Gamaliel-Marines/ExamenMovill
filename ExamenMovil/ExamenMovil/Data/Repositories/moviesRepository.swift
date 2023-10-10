6c8d61ac64436e59e20d277009f634c0289cb64d
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-10-10 12:57:34 -0600

    Feat: sample view

--- ExamenMovil/ExamenMovil/Data/Repositories/moviesRepository.swift
+++ ExamenMovil/ExamenMovil/Data/Repositories/moviesRepository.swift
@@ -1,26 +1,25 @@
-//
-//  moviesRepository.swift
-//  ExamenMovil
-//
-//  Created by Gamaliel Marines on 10/10/23.
-//
-
 import Foundation
 
+// Protocol that defines the interface for fetching popular movies
 protocol MoviesRepository {
     func fetchPopularMovies(completion: @escaping (Result<[Movie], Error>) -> Void)
 }
 
+// Enum representing possible network errors
 enum NetworkError: Error {
     case invalidURL
     case invalidResponse
     case noData
 }
 
+// Concrete implementation of the MoviesRepository protocol
 class MoviesRepositoryImpl: MoviesRepository {
-    private let apiKey = "https://api.themoviedb.org/3/movie/popular"
+    // Authentication key for the movie API (Not recommended to store it in the code!)
+    private let apiKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjhkZDc1NmUzMmI4YWNlZjYyZmQ2YzMwZmQwY2NmOSIsInN1YiI6IjY0ZWI9MzhiZTg5NGE2MDEzYmIxNjNjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6cGDTI8wql15qnVZErrd_6QRNaiRAi74pRD0LfOzVZM"
     
+    // Method to fetch popular movies
     func fetchPopularMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        // Build the URL for the request to fetch popular movies
         guard let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=\(apiKey)") else {
             completion(.failure(NetworkError.invalidURL))
             return
@@ -29,27 +28,34 @@ class MoviesRepositoryImpl: MoviesRepository {
         // Perform the HTTP request
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
+                // In case of an error in the request, call the completion closure with the error
                 completion(.failure(error))
                 return
             }
             
+            // Check if the HTTP response is valid (status code 200)
             guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
                 completion(.failure(NetworkError.invalidResponse))
                 return
             }
             
+            // Check if data was received in the response
             guard let data = data else {
                 completion(.failure(NetworkError.noData))
                 return
             }
             
             do {
+                // Decode the JSON response into a MovieResponse object
                 let decoder = JSONDecoder()
                 let movieResponse = try decoder.decode(MovieResponse.self, from: data)
+                
+                // Call the completion closure with the list of movies as a success
                 completion(.success(movieResponse.results))
             } catch {
+                // In case of an error in decoding, call the completion closure with the error
                 completion(.failure(error))
             }
-        }.resume()
+        }.resume() // Start the HTTP request task
     }
 }
--- ExamenMovil/ExamenMovil/Presentation/ModelViews/moviesViewModel.swift
+++ ExamenMovil/ExamenMovil/Presentation/ModelViews/moviesViewModel.swift
@@ -7,25 +7,33 @@
 
 import Foundation
 
+// ViewModel for managing a list of movies
 class MovieListViewModel: ObservableObject {
+    // Published property to notify views when the movies array changes
     @Published var movies = [Movie]()
     
+    // Reference to the MoviesRepository used to fetch movie data
     private let movieRepository: MoviesRepository
     
+    // Initializer to inject the MoviesRepository dependency
     init(movieRepository: MoviesRepository) {
         self.movieRepository = movieRepository
     }
     
+    // Function to load data from the movie repository
     func loadData() {
+        // Use the movie repository to fetch popular movies
         movieRepository.fetchPopularMovies { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .success(let fetchedMovies):
+                // Update the movies array on the main thread
                 DispatchQueue.main.async {
                     self.movies = fetchedMovies
                 }
             case .failure(let error):
+                // Handle and log any errors that occur during data loading
                 print("Error loading movies: \(error.localizedDescription)")
             }
         }
