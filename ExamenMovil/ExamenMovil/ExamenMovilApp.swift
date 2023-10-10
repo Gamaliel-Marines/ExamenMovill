7cd136cbda133b75ef9707cc8ab2a1d23da404a3
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-10-10 11:15:58 -0600

    Feat: creation of project

---
+++ ExamenMovil/ExamenMovil/ExamenMovilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenMovilApp.swift
+//  ExamenMovil
+//
+//  Created by Gamaliel Marines on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamenMovilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
